d9dd4687a287d0c8023bba8e12c6fa95777c63ed
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-05-25 16:24:45 +0200

    Add first XCode project

---
+++ LiftWeights/LiftWeights/LiftWeightsApp.swift
@@ -0,0 +1,20 @@
+//
+//  LiftWeightsApp.swift
+//  LiftWeights
+//
+//  Created by Andrea on 25/05/22.
+//
+
+import SwiftUI
+
+@main
+struct LiftWeightsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
