e7787d72ddd383f125e6ed14000439b1d135d386
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2022-05-27 19:02:27 +0200

    Update launcher

---
+++ LiftWeights/LiftWeights/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  LiftWeights
+//
+//  Created by Andrea on 27/05/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- LiftWeights/LiftWeights/Views/LauncherView.swift
+++ LiftWeights/LiftWeights/Views/LauncherView.swift
@@ -6,19 +6,39 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct LauncherView: View {
+    @State var animationFinished = false
+    
     var body: some View {
-        VStack(alignment: .center) {
-            Image("icon")
-                .resizable()
-                .frame(width: 150, height: 150)
-            Text("LiftWeights")
-                .font(.title)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
+        NavigationView {
+            NavigationLink(destination: HomeView()) {
+                ZStack(alignment: .center) {
+                    Color(red: 28/255, green: 28/255, blue: 28/255)
+                        .ignoresSafeArea(.all)
+                    VStack(alignment: .center) {
+                        Image("logo")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                        Text("LiftWeights")
+                            .font(.title)
+                            .fontWeight(.heavy)
+                            .foregroundColor(Color.white)
+                        //AnimatedImage(url: <#T##URL?#>)
+                    }
+                }.ignoresSafeArea(.all)
+            }
         }
     }
+    /*
+    private func getLogoURL()->URL{
+        let bundle = Bundle.main.path(forResource: "logo", ofType: <#T##String?#>)
+        let url = URL(fileURLWithPath: bundle ?? "")
+        
+        return url
+    }
+    */
 }
 
 
